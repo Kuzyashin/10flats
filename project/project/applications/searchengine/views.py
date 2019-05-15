e136da86c01e2df8f4ee24d9d82316da37f53264
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2019-05-15 22:47:26 +0300

    Step 1

--- project/project/applications/searchengine/views.py
+++ project/project/applications/searchengine/views.py
@@ -1,12 +1,14 @@
-import json
 from django.utils import timezone
-from .models import Search
-from properites.models import Area
 from rest_framework.response import Response
-from rest_framework import authentication, permissions
-from rest_framework import viewsets, views, status
-from rest_framework.decorators import api_view
+from rest_framework import permissions
+from rest_framework import views
+
+from .models import Search
+
+from realty.models import RealtyObject
+from properites.models import Area
 from properites.serializers import AreaSerializer
+
 # Create your views here.
 import logging
 
@@ -29,8 +31,8 @@ class SearchViewSet(views.APIView):
 
     def post(self, request, format=None):
         data = request.data
+        user_id = data.get('user_id')
         if data.get('step') == '0':
-            user_id = data.get('user_id')
             search = Search.objects.create(
                 user_identify=user_id,
                 created_at=timezone.now(),
@@ -41,7 +43,38 @@ class SearchViewSet(views.APIView):
             serialized = AreaSerializer(area_list, many=True)
             logger.info(serialized.data)
             resp_data = {"step": 1,
-                         "answers": serialized.data}
+                         "answers": serialized.data,
+                         "count": RealtyObject.objects.all().count()}
             return Response(data=resp_data, status=200)
+        elif data.get('step') == '1':
+            search = Search.objects.filter(
+                user_identify=user_id,
+                last_step=1
+            ).last()
+            areas_pk = data.get('data')
+            search.step_1 = areas_pk
+            search.last_step = 2
+            search.save()
+            realty_objects = RealtyObject.objects.filter(realty_complex__area_id__in=areas_pk)
+            count = realty_objects.count()
+            room_list = realty_objects.distinct('rooms_count').values('rooms_count')
+            resp_data = {"step": 2,
+                         "answers": room_list,
+                         "count": count}
+            return Response(data=resp_data, status=200)
+        elif data.get('step') == '2':
+            pass
+        elif data.get('step') == '3':
+            pass
+        elif data.get('step') == '4':
+            pass
+        elif data.get('step') == '5':
+            pass
+        elif data.get('step') == '6':
+            pass
+        elif data.get('step') == '7':
+            pass
+        elif data.get('step') == '8':
+            pass
         else:
             return Response(request.data)
