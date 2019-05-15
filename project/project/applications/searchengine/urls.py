a7669f4941ce0d4445c0c0758e1cf3b66239a24e
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2019-05-15 20:47:42 +0300

    Test DRF

--- project/project/applications/searchengine/urls.py
+++ project/project/applications/searchengine/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
 
-router.register(r'search', SearchViewSet)
+router.register(r'search', SearchViewSet, basename='search_view')
 
 urlpatterns = router.urls
