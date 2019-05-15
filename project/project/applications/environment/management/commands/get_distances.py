3c3f79bfbc00bf82b6d294abe43e3486d732c9b6
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2019-05-16 00:39:58 +0300

    fix

--- project/project/applications/environment/management/commands/get_distances.py
+++ project/project/applications/environment/management/commands/get_distances.py
@@ -16,27 +16,27 @@ class Command(BaseCommand):
         token = os.environ['GOOGLE_API_KEY']
         complex_pk = options['complex_pk']
         gmaps = googlemaps.Client(key=token)
+        i=0
+        compl = RealtyComplex.objects.get(pk=int(complex_pk))
         places_count = Place.objects.all().count()
-        for compl in RealtyComplex.objects.get(pk=int(complex_pk)):
-            i = 0
-            for place in Place.objects.all():
-                i += 1
-                try:
-                    DistanceMatrix.objects.get(place=place, complex=compl)
-                except DistanceMatrix.MultipleObjectsReturned:
-                    print('Need to FIX THIS SHEET', DistanceMatrix.objects.filter(place=place, complex=compl))
-                except DistanceMatrix.DoesNotExist:
-                    result = gmaps.distance_matrix(
-                        origins=(compl.lat, compl.lng),
-                        destinations=(place.lat, place.lng),
-                        mode='walking',
-                        units='metric'
-                    )
-                    DistanceMatrix.objects.create(
-                        complex=compl,
-                        place=place,
-                        distance=result.get('rows')[0].get('elements')[0].get('distance').get('value'),
-                        duration=result.get('rows')[0].get('elements')[0].get('duration').get('value'),
-                    )
-                print('Current {} / Total {} / {}'.format(i, places_count, compl.name))
+        for place in Place.objects.all():
+            i += 1
+            try:
+                DistanceMatrix.objects.get(place=place, complex=compl)
+            except DistanceMatrix.MultipleObjectsReturned:
+                print('Need to FIX THIS SHEET', DistanceMatrix.objects.filter(place=place, complex=compl))
+            except DistanceMatrix.DoesNotExist:
+                result = gmaps.distance_matrix(
+                    origins=(compl.lat, compl.lng),
+                    destinations=(place.lat, place.lng),
+                    mode='walking',
+                    units='metric'
+                )
+                DistanceMatrix.objects.create(
+                    complex=compl,
+                    place=place,
+                    distance=result.get('rows')[0].get('elements')[0].get('distance').get('value'),
+                    duration=result.get('rows')[0].get('elements')[0].get('duration').get('value'),
+                )
+            print('Current {} / Total {} / {}'.format(i, places_count, compl.name))
 
