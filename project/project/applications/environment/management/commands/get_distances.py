fbd90db990b9df172496cc4336a1de55d33c2a8e
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2019-05-16 14:27:06 +0300

    final ?

--- project/project/applications/environment/management/commands/get_distances.py
+++ project/project/applications/environment/management/commands/get_distances.py
@@ -11,32 +11,34 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('complex_pk')
+        parser.add_argument('complex_pk_end')
 
     def handle(self, *args, **options):
         token = os.environ['GOOGLE_API_KEY']
         complex_pk = options['complex_pk']
         gmaps = googlemaps.Client(key=token)
-        i=0
-        compl = RealtyComplex.objects.get(pk=int(complex_pk))
-        places_count = Place.objects.all().count()
-        for place in Place.objects.all():
-            i += 1
-            try:
-                DistanceMatrix.objects.get(place=place, complex=compl)
-            except DistanceMatrix.MultipleObjectsReturned:
-                print('Need to FIX THIS SHEET', DistanceMatrix.objects.filter(place=place, complex=compl))
-            except DistanceMatrix.DoesNotExist:
-                result = gmaps.distance_matrix(
-                    origins=(compl.lat, compl.lng),
-                    destinations=(place.lat, place.lng),
-                    mode='walking',
-                    units='metric'
-                )
-                DistanceMatrix.objects.create(
-                    complex=compl,
-                    place=place,
-                    distance=result.get('rows')[0].get('elements')[0].get('distance').get('value'),
-                    duration=result.get('rows')[0].get('elements')[0].get('duration').get('value'),
-                )
-            print('Current {} / Total {} / {}'.format(i, places_count, compl.address))
+        for ids in range(options['complex_id'], options['complex_id_end']):
+            i=0
+            compl = RealtyComplex.objects.get(pk=int(complex_pk))
+            places_count = Place.objects.all().count()
+            for place in Place.objects.all():
+                i += 1
+                try:
+                    DistanceMatrix.objects.get(place=place, complex=compl)
+                except DistanceMatrix.MultipleObjectsReturned:
+                    print('Need to FIX THIS SHEET', DistanceMatrix.objects.filter(place=place, complex=compl))
+                except DistanceMatrix.DoesNotExist:
+                    result = gmaps.distance_matrix(
+                        origins=(compl.lat, compl.lng),
+                        destinations=(place.lat, place.lng),
+                        mode='walking',
+                        units='metric'
+                    )
+                    DistanceMatrix.objects.create(
+                        complex=compl,
+                        place=place,
+                        distance=result.get('rows')[0].get('elements')[0].get('distance').get('value'),
+                        duration=result.get('rows')[0].get('elements')[0].get('duration').get('value'),
+                    )
+                print('Current {} / Total {} / {}'.format(i, places_count, compl.pk))
 
--- project/project/applications/environment/management/commands/get_google_places.py
+++ project/project/applications/environment/management/commands/get_google_places.py
@@ -13,6 +13,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('complex_id')
+        parser.add_argument('complex_id_end')
 
     def get_places(self, token, next_token, place_type, lat, lng):
         time.sleep(3)
@@ -25,7 +26,6 @@ class Command(BaseCommand):
                 page_token=next_token
             )
             next_page_token = places.get('next_page_token', None)
-            print(next_page_token)
             results = places.get('results')
             if results:
                 for place in results:
@@ -65,47 +65,49 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         token = os.environ['GOOGLE_API_KEY']
         types = PlaceType.objects.all()
-        realty_complex = RealtyComplex.objects.get(pk=options['complex_id'])
         gmaps = googlemaps.Client(key=token)
 
-        for place_type in types:
-            places = gmaps.places_nearby(
-                location=(realty_complex.lat, realty_complex.lng),
-                radius=5000,
-                type=place_type
-            )
-            next_page_token = places.get('next_page_token', None)
-            print(next_page_token)
-            results = places.get('results')
-            if results:
-                for place in results:
-                    try:
-                        Place.objects.get(google_place_id=place.get('place_id'))
-                    except Place.DoesNotExist:
-                        if place.get('plus_code'):
-                            new_place = Place.objects.create(
-                                name=place.get('name'),
-                                lat=place.get('geometry').get('location').get('lat'),
-                                lng=place.get('geometry').get('location').get('lng'),
-                                google_place_id=place.get('place_id', None),
-                                google_plus_code=place.get('plus_code').get('compound_code', None),
-                                address=place.get('vicinity', None),
-                            )
-                        else:
-                            new_place = Place.objects.create(
-                                name=place.get('name'),
-                                lat=place.get('geometry').get('location').get('lat'),
-                                lng=place.get('geometry').get('location').get('lng'),
-                                google_place_id=place.get('place_id', None),
-                                address=place.get('vicinity', None),
-                            )
-                        for pl_type in place.get('types'):
-                            try:
-                                new_place.place_type.add(PlaceType.objects.get(type=pl_type))
-                            except PlaceType.DoesNotExist:
-                                new_type = PlaceType.objects.create(type=pl_type)
-                                new_type.save()
-                                new_place.place_type.add(PlaceType.objects.get(type=pl_type))
-                        new_place.save()
-            if next_page_token:
-                self.get_places(token, next_page_token, place_type, realty_complex.lat, realty_complex.lng)
+        for i in range(options['complex_id'], options['complex_id_end']):
+            print(i)
+            realty_complex = RealtyComplex.objects.get(pk=options['complex_id'])
+
+            for place_type in types:
+                places = gmaps.places_nearby(
+                    location=(realty_complex.lat, realty_complex.lng),
+                    radius=5000,
+                    type=place_type
+                )
+                next_page_token = places.get('next_page_token', None)
+                results = places.get('results')
+                if results:
+                    for place in results:
+                        try:
+                            Place.objects.get(google_place_id=place.get('place_id'))
+                        except Place.DoesNotExist:
+                            if place.get('plus_code'):
+                                new_place = Place.objects.create(
+                                    name=place.get('name'),
+                                    lat=place.get('geometry').get('location').get('lat'),
+                                    lng=place.get('geometry').get('location').get('lng'),
+                                    google_place_id=place.get('place_id', None),
+                                    google_plus_code=place.get('plus_code').get('compound_code', None),
+                                    address=place.get('vicinity', None),
+                                )
+                            else:
+                                new_place = Place.objects.create(
+                                    name=place.get('name'),
+                                    lat=place.get('geometry').get('location').get('lat'),
+                                    lng=place.get('geometry').get('location').get('lng'),
+                                    google_place_id=place.get('place_id', None),
+                                    address=place.get('vicinity', None),
+                                )
+                            for pl_type in place.get('types'):
+                                try:
+                                    new_place.place_type.add(PlaceType.objects.get(type=pl_type))
+                                except PlaceType.DoesNotExist:
+                                    new_type = PlaceType.objects.create(type=pl_type)
+                                    new_type.save()
+                                    new_place.place_type.add(PlaceType.objects.get(type=pl_type))
+                            new_place.save()
+                if next_page_token:
+                    self.get_places(token, next_page_token, place_type, realty_complex.lat, realty_complex.lng)
