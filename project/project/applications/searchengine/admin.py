317b5136bf50a4843fdbca697106e5bc4ac01155
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2019-05-16 12:48:30 +0300

    final ?

--- project/project/applications/searchengine/admin.py
+++ project/project/applications/searchengine/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import DistanceChoose, Search
+from .models import DistanceChoose, Search, PercentPass
 
 # Register your models here.
 
 admin.site.register(Search)
+admin.site.register(PercentPass)
 admin.site.register(DistanceChoose)
