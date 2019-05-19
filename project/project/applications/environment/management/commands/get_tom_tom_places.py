d654b65ea9cbcfc7388889a8597d0d1cad116ed9
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2019-05-19 21:53:16 +0300

    commit

--- project/project/applications/environment/management/commands/get_tom_tom_places.py
+++ project/project/applications/environment/management/commands/get_tom_tom_places.py
@@ -19,12 +19,12 @@ class Command(BaseCommand):
         maps = TomTom(token=os.environ['TOMTOM_API_KEY'])
         start_cat_list = [7372, 9362, 7332, 9361051, 9376, 7320002]
         for realty_complex in RealtyComplex.objects.all():
-            maps.default_radius = 2000
+            maps.default_radius = '2000'
             places_data = maps.get_nearby(
                 lat=realty_complex.lat,
                 lng=realty_complex.lng,
                 category='7372,9362,7332,9361051,9376,7320002',
-                offset=places_on_page+current_offset
+                offset='{}'.format(places_on_page+current_offset)
             )
             places_total = places_data.get('summary').get('totalResults')
             places_on_page = places_data.get('summary').get('numResults')
@@ -49,18 +49,18 @@ class Command(BaseCommand):
                         tom_place.childCategory.add(category)
                     tom_place.save()
             if places_total > places_on_page + current_offset:
-                self.get_more_places(places_total, places_on_page, current_offset)
+                self.get_more_places( places_on_page, current_offset)
 
     def handle(self, *args, **options):
         maps = TomTom(token=os.environ['TOMTOM_API_KEY'])
         start_cat_list = [7372, 9362, 7332, 9361051, 9376, 7320002]
         for realty_complex in RealtyComplex.objects.all():
-            maps.default_radius = 2000
+            maps.default_radius = '2000'
             places_data = maps.get_nearby(
                 lat=realty_complex.lat,
                 lng=realty_complex.lng,
                 category='7372,9362,7332,9361051,9376,7320002',
-                offset=0
+                offset='0'
             )
             places_total = places_data.get('summary').get('totalResults')
             places_on_page = places_data.get('summary').get('numResults')
