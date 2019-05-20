b661b94db7e289d5e526b8632dc564c6b2490ce0
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2019-05-20 22:16:23 +0300

    add sleep

--- project/project/applications/core/utils/TomTom.py
+++ project/project/applications/core/utils/TomTom.py
@@ -19,14 +19,36 @@ class TomTom:
     def get_categories(self):
         url = self.base_url + '/search/2/poiCategories.' + self.default_format + '?key=' + self.token
         data = requests.get(url)
-        return data.json()
+        try:
+            return data.json()
+        except Exception as e:
+            time.sleep(1)
+            logger.warning(e)
+            logger.warning(data)
+            try:
+                return json.loads(data)
+            except Exception as e:
+                logger.warning(e)
+                time.sleep(3)
+                return requests.get(url).json()
 
     def get_nearby(self, lat, lng, category, offset):
         url = self.base_url + '/search/2/nearbySearch/.' + self.default_format + '?lat={}&lon={}'.format(lat, lng) + \
             '&limit=' + self.default_limit + '&categorySet=' + category + '&ofs={}'.format(offset) + '&radius=' \
               + self.default_radius + '&idxSet=POI' + '&key=' + self.token
         data = requests.get(url)
-        return data.json()
+        try:
+            return data.json()
+        except Exception as e:
+            time.sleep(1)
+            logger.warning(e)
+            logger.warning(data)
+            try:
+                return json.loads(data)
+            except Exception as e:
+                logger.warning(e)
+                time.sleep(3)
+                return requests.get(url).json()
 
     def get_route(self, start_lat, start_lng, fnish_lat, fnish_lng):
         url = self.base_url \
