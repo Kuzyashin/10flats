7aa3fe19fef3712f11cd6be0462a147ff27db61e
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2019-05-21 13:47:41 +0300

    Add Nearby objects FIX

--- project/project/applications/environment/serializers.py
+++ project/project/applications/environment/serializers.py
@@ -7,12 +7,16 @@ class TomTomPOISerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TomTomPOI
-        fields = ('name',)
+        fields = ('name', )
 
 
 class TomTomPlaceSerializer(serializers.ModelSerializer):
-    place_type = TomTomPOISerializer()
+    place_type = TomTomPOISerializer(many=True, read_only=True)
+    address = serializers.SerializerMethodField()
 
     class Meta:
         model = TomTomPlace
         fields = ('place_type', 'name', 'address', 'lat', 'lng', )
+
+    def get_address(self, obj):
+        return [obj.address]
