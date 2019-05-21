d8f6ee5492dd686f154dddc13b58c6da9f498bde
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2019-05-21 13:12:30 +0300

    Add Nearby objects FIX

--- project/project/applications/core/serializers.py
+++ project/project/applications/core/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
 from .models import TomTomDistanceMatrix
 from environment.serializers import TomTomPlaceSerializer
-
+from environment.models import TomTomPlace
 
 class TomTomDistanceMatrixSerializer(serializers.ModelSerializer):
-    place = TomTomPlaceSerializer()
+    # place = TomTomPlaceSerializer()
+    place = serializers.SerializerMethodField()
 
     class Meta:
         model = TomTomDistanceMatrix
-        fields = ('place', 'distance', 'duration', 'route', )
+        fields = ('place', 'distance', 'duration', )
+
+    def get_place(self, obj):
+        return {
+            "name": obj.place.name,
+            "address": obj.place.address,
+            "lat": obj.place.lat,
+            "lng": obj.place.lng
+            }
